d540e419ae96af5f3b73caac23cedfec4cda490a
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-12-16 14:52:37 +0300

    implement REPL functionality and update main entry point

--- repl.go
+++ repl.go
@@ -1,6 +1,31 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		if err := reader.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
