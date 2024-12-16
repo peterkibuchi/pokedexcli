813aa28041691f457917fc67bfcc2763df237e5e
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-12-16 16:50:09 +0300

    refactor command handling for exit and help in REPL

---
+++ command_help.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	var cmdOutput string
+	for _, cmd := range getSupportedCommands() {
+		cmdOutput += fmt.Sprintf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Printf("\nWelcome to the Pokedex!\nUsage:\n\n%s\n", cmdOutput)
+	return nil
+}
--- repl.go
+++ repl.go
@@ -43,3 +43,24 @@ func cleanInput(text string) []string {
 	words := strings.Fields(output)
 	return words
 }
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getSupportedCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
